638fb8e0f7cb89cb73e98ac4c67e740198b7bffd
synthresin <synthresin@me.com>
2012-09-23 21:34:58 +0900

    프로젝트 시작

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rapunzel87::Application.config.secret_token = 'b5aee57fd891f721a20c66662df7dd1d37c18e95e2f1215518eb0115d4bdf0033050596defd5d89d843cd9654f3bbd06666fc62955aedeb5514402679181716b'
