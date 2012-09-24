11f3956e51998c89e23d558f153bce34242b78f5
synthresin <synthresin@me.com>
2012-09-24 13:33:58 +0900

    여러가지 셋업들

--- app/controllers/guestbook_controller.rb
+++ app/controllers/guestbook_controller.rb
@@ -1,4 +1,13 @@
 class GuestbookController < ApplicationController
 	def index
+		
+		if signed_in?
+			@micropost = current_user.microposts.build
+		else
+			@micropost = Micropost.new
+		end
+
+		@microposts = Microposts.all
+		
 	end
 end
