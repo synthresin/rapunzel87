a2ef7abe8cecc47e6921b636f7e5ad61fbeb895f
synthresin <synthresin@me.com>
2012-09-24 13:13:28 +0900

    게스트북 컨트롤러

---
+++ app/controllers/guestbook_controller.rb
@@ -0,0 +1,2 @@
+class GuestbookController < ApplicationController
+end
---
+++ test/functional/guestbook_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class GuestbookControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
