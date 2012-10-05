12780ef2e9295ac91d908004a739e0742b15ebed
synthresin <synthresin@me.com>
2012-10-05 15:51:26 +0900

    ember rails 디렉토리 구조 생성

---
+++ app/assets/javascripts/ember/rapunzel87.js
@@ -0,0 +1,8 @@
+#= require_self
+#= require_tree ./models
+#= require_tree ./controllers
+#= require_tree ./views
+#= require_tree ./helpers
+#= require_tree ./templates
+
+Rapunzel87 = Ember.Application.create();
